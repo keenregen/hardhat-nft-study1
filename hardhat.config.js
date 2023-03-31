d5a9d8e6c5dda106f6e9f2ccfc778dcef565f83e
Keen Regen <keenregen@gmail.com>
2023-04-01 02:22:13 +0300

    v2.5

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,15 +3,15 @@ require("dotenv").config({ path: ".env" });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    alfajores: {
-      url: "https://alfajores-forno.celo-testnet.org",
-      accounts: {
-        mnemonic: process.env.MNEMONIC, // line 25
-        path: "m/44'/60'/0'/0", // line 26
-      },
-      chainId: 44787,
-    },
-  },
+     solidity: "0.8.4",
+     networks: {
+          alfajores: {
+               url: "https://alfajores-forno.celo-testnet.org",
+               accounts: {
+                    mnemonic: process.env.MNEMONIC, // line 25
+                    path: "m/44'/60'/0'/0", // line 26
+               },
+               chainId: 44787,
+          },
+     },
 };
